47e050d5be871af874ad7d179d1fc9696ee55416
wacky556 <william.a.macke@gmail.com>
2020-11-25 11:39:02 -0600

    added code for random_normal initialization

---
+++ src/initializer/random_normal.h
@@ -0,0 +1,29 @@
+#ifndef RANDOM_NORMAL_H_
+#define RANDOM_NORMAL_H_
+
+#include <random>
+#include <chrono>
+
+struct random_normal_init {
+	const float mean;
+	const float std;
+	random_normal_init(float mean, float std) : mean(mean), std(std) { }
+	~random_normal_init() { }
+	template <class T>
+	void initialize(Matrix<T>& mat, size_t num_nodes, size_t num_inputs, size_t num_outputs) {
+		float* data = new float[mat.getN()*mat.getM()];
+
+		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  		std::default_random_engine generator (seed);
+		for (int i = 0; i < mat.getN()*mat.getM(); ++i) {
+			data[i] = std::normal_distribution<float>(mean, std);
+		}
+
+		mat.setValues(data);
+
+		delete[] data;
+	}
+};
+
+
+#endif
