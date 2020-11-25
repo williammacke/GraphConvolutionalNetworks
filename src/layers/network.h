d4dcd0aec363b4ac1968c6abd7995a0764083489
wacky556 <william.a.macke@gmail.com>
2020-11-25 11:29:54 -0600

    fixed some issues with training function in network

--- src/layers/network.h
+++ src/layers/network.h
@@ -8,7 +8,7 @@
 template <class Loss, class Trainer>
 class Network {
 public:
-	Network(const std::vector<GCNLayer>& layers, const Loss& loss, const Trainer& trainer, cublasHandle_t bhandle, cusparseHandle_t shandle) : layers(layers), loss(loss), trainer(trainer), g(nullptr), bhandle(bhandle), shandle(shandle) {
+	Network(size_t num_nodes, size_t num_output_features, const std::vector<GCNLayer>& layers, const Loss& loss, const Trainer& trainer, cublasHandle_t bhandle, cusparseHandle_t shandle) : layers(layers), loss(loss), trainer(trainer), g(nullptr), bhandle(bhandle), shandle(shandle), ld(num_nodes, num_output_features), y(num_nodes, num_output_features) {
 
 	}
 	~Network() { }
@@ -25,12 +25,19 @@ public:
 	}
 	void train(size_t num_epochs, const Matrix<float>& in) {
 		size_t num_layers = layers.size();
+		size_t last = num_layers-1;
 		for (int i = 0; i < num_epochs; ++i) {
 			result(in);
-			for (int j = num_layers-1; j >= 0; --j) {
-				trainer.optimize(sHandle, bHandle, layers[j]);
+			loss.derivative(bHandle, y, layers[last].getOut(), ld);
+			trainer.optimize(sHandle, bHandle, layers[last], ld);
+			for (int j = last-1; j >= 0; --j) {
+				trainer.optimize(sHandle, bHandle, layers[j], layers[j+1].getNext());
 			}
 		}
+	}
+
+	void setLabels(float* labels) {
+		y.setValues(labels);
 	}
 private:
 	std::vector<GCNLayer> layers;
@@ -39,6 +46,8 @@ private:
 	cublasHandle_t bhandle;
 	cusparsehandle_t shandle;
 	Graph* g;
+	Matrix<float> y;
+	Matrix<float> ld;
 
 };
 
