7017507f04875ecaaecfc9d1bd578b2940c26b6f
ryan <ryanjrock95@gmail.com>
2020-12-04 19:13:03 -0600

    Add glorot initializer

--- src/initializer/random_normal.h
+++ src/initializer/random_normal.h
@@ -27,4 +27,29 @@ struct random_normal_init {
 };
 
 
+struct glorot {
+	const float mean;
+	const float std;
+
+	glorot(float mean, float std) : mean(mean), std(std) {}
+
+	~glorot() {}
+
+	void initialize(Matrix<float>& mat, size_t num_nodes, size_t num_inputs,
+					size_t num_outputs) const {
+		float *data = new float[mat.getN()*mat.getM()];
+		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+		std::default_random_engine generator(seed);
+		float lo = -std::sqrt(6) / std::sqrt(num_inputs + num_outputs);
+		float hi = std::sqrt(6) / std::sqrt(num_inputs + num_outputs);
+		std::uniform_real_distribution<float> dist(lo, hi);
+		for (int i = 0; i < mat.getN()*mat.getM(); ++i) {
+			data[i] = dist(generator);
+		}
+		mat.setValues(data);
+		delete[] data;
+	}
+};
+
+
 #endif
